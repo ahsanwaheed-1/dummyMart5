1a54c4320c35a82d9dbca159b1d0c9497d24ba67
Ahsan Waheed <112880995+ahsanwaheed-1@users.noreply.github.com>
2025-12-01 20:22:22 +0500

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,12 +1,5 @@
 import mongoose from 'mongoose';
 
-const reviewSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  rating: { type: Number, required: true, min: 1, max: 5 },
-  comment: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
-
 const productSchema = new mongoose.Schema({
   name: { 
     type: String, 
@@ -23,20 +16,27 @@ const productSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
-  rating: { 
-    type: Number, 
-    required: true,
-    min: 0,
-    max: 5,
-    default: 0
-  },
   description: { 
     type: String, 
     required: true,
     trim: true
   },
-  reviews: [reviewSchema],
   imageUrl: { 
+    type: String, 
+    required: true,
+    trim: true
+  },
+  store: { 
+    type: String, 
+    required: true,
+    trim: true
+  },
+  brand: { 
+    type: String, 
+    required: true,
+    trim: true
+  },
+  email: { 
     type: String, 
     required: true,
     trim: true
